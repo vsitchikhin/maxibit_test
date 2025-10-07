47e2f24c029a6be576f0d5b89bd1c92b1fc95b3d
Vladimir Sitchikhin <vsitchikhin@gmail.com>
2025-10-07 23:48:39 +0300

    feat(404): Добавлена простенькая 404

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="not-found">404</div>
+  <div class="d-flex flex-column justify-center align-center">
+    <div class="d-flex align-center justify-center w-100 h-100 mb-8">
+      <v-img src="404.png" />
+    </div>
+    <v-btn type="text" text="Go to main page" color="accent" @click="gotoMainPage" />
+  </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+    function gotoMainPage() {
+      router.push('/');
+    }
+
+    return {
+      gotoMainPage,
+    };
+  },
+});
 </script>
