5a9d5867a5ee8f100acedcb25117320e48282404
Vladimir Sitchikhin <vsitchikhin@gmail.com>
2025-10-08 00:30:36 +0300

    fix(404): Небольшой фикс 404

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex flex-column justify-center align-center">
-    <div class="d-flex align-center justify-center w-100 h-90 mb-8">
+    <div class="d-flex align-center justify-center w-90 h-90 mb-8">
       <v-img src="404.png" />
     </div>
     <v-btn type="text" text="Go to main page" color="accent" @click="gotoMainPage" />
